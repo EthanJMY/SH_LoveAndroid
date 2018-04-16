656edef3fdce72b501694e021dbc8a015c0f977a
EthanJi <946446901@qq.com>
2018-04-16 22:47:51 +0800

    Initial commit

---
+++ app/src/main/java/com/sh/ethan/sh_loveandroid/activity/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.sh.ethan.sh_loveandroid.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.sh.ethan.sh_loveandroid.R;
+
+/**
+ * Created by ethan on 2018/4/16.
+ * 欢迎页
+ */
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
